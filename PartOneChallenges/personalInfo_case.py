e70478b9b99c09109ff1936928ece0a7af525e5a
pythagoras58 <solomondanso58@gmail.com>
2023-09-13 23:25:24 +0800

    Initial commit

---
+++ PartOneChallenges/personalInfo_case.py
@@ -0,0 +1,3 @@
+babes_name = 'Cutie'
+
+print(f'Hello {babes_name}, would you like to learn some Python today?')
---
+++ variable_strings.py
@@ -0,0 +1,5 @@
+full_url = "https://www.github.com/Pythagoras58"
+
+base_url = full_url.removeprefix('https://')
+
+print(f'The base url is : {base_url}')
