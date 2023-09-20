643c4b0dd5caa0345e011c815e0ed48791d51491
pythagoras58 <solomondanso58@gmail.com>
2023-09-20 12:12:03 +0800

    Part 2 challenges

---
+++ PartTwoChallenges/guest_list.py
@@ -0,0 +1,15 @@
+"""
+    If you could invite anyone, living or deceased, to dinner, who
+would you invite? Make a list that includes at least three people you’d like to
+invite to dinner. Then use your list to print a message to each person, inviting
+them to dinner.
+"""
+
+guest_list = []
+
+guest_list.append('Amoah')
+guest_list.append('Danso')
+guest_list.append('Appiah')
+
+for i in guest_list:
+    print(f'Hi {i}, you are invited to my party tonight')
--- lists.py
+++ lists.py
@@ -19,3 +19,15 @@ print(f'New List of Arrays : {motorcycles}')
 # insert at a location
 motorcycles.insert(0, 'Corrido')
 print(f'New List of Arrays : {motorcycles}')
+
+# remove an item from a list
+del motorcycles[3]
+print(f'New List of Arrays - Del : {motorcycles}')
+
+popped_list = motorcycles.pop()
+print(f'New List of Arrays - Pop : {popped_list}')
+
+#Removing an Item by Value
+motorcycles_rem = ['honda', 'yamaha', 'suzuki', 'ducati']
+motorcycles_rem.remove('ducati')
+print(motorcycles_rem)
