d6e7bdfe067f59b41fe0be3043851613063641d1
pythagoras58 <solomondanso58@gmail.com>
2023-09-22 00:54:35 +0800

    Comparison

---
+++ operations.py
@@ -0,0 +1,11 @@
+# Arithmetics operations
+print(f'Add : {120 + 54}')
+print(f'Subtract : {120 - 54}')
+print(f'Multiplication : {120 * 54}')
+print(f'Division : {120 / 54}')
+print(f'Modulo : {120 % 54}')
+print(f'Divisor : {120 // 54}')
+print(f'Exponentiation : {12 ** 5}')
+
+
+# Comparison operators
