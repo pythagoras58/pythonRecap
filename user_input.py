8819986a91369e5fca96dbbf1385bb2228ff6b9d
pythagoras58 <solomondanso58@gmail.com>
2023-09-22 00:28:07 +0800

    Numbers complete

--- user_input.py
+++ user_input.py
@@ -11,4 +11,11 @@ print(type(y))
 print(type(z))
 
 
-#=====Importation
+## =====Importation
+
+import random
+print(f'Random Number: {random.random()}')
+print(f'Randint Number : {random.randint(1, 50)}')
+print(f'Random Choice Number : {random.choice([43,32,3,4,4,543,355,342])}') # make one choice from a list
+print(f'Random Sample: {random.sample([45,342,32,12,34,46,78,9,7,6],5)}') # make sample choices to make a list
+
