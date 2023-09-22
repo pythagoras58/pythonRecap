0dcf06585dafa02b1cdffc54e738b9834f1e10f8
pythagoras58 <solomondanso58@gmail.com>
2023-09-22 12:51:27 +0800

    Data structures

--- operations.py
+++ operations.py
@@ -9,3 +9,9 @@ print(f'Exponentiation : {12 ** 5}')
 
 
 # Comparison operators
+print(f'Is 3 equal to 3: {3 == 3}')
+
+# LOGICAL PROCESSES AND CHOICES
+print(f'5 == 5 and 10 ==10: {5 == 5 and 10 ==10}')
+print(f'5 == 5 and 10 ==10: {5 == 5 or 10 ==10}')
+print(f'5 == 5 and 10 ==10: {5 == 5 and 10 ==10}')
