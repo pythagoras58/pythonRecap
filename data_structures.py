3d76dcd18def220d263464e04a85d1797646803a
pythagoras58 <solomondanso58@gmail.com>
2023-09-23 00:41:35 +0800

    Data structures - 1

---
+++ data_structures.py
@@ -0,0 +1,11 @@
+# LISTS
+players = ['Ronaldo','Ronaldinho','Kaka', 'Cafu']
+
+print(f'The list of players: {players}')
+
+mixed = ['Neymar',[2,3,4],23,True]
+print(f'Mixed list: {mixed}')
+
+
+# ==== Methods =====
+print(f'Lenght : {len(players)}') # get the lenght of the list
