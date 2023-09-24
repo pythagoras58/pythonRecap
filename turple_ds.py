b1740d85d36a5dcbf7ec2cb72f87f7a89d2d438b
pythagoras58 <solomondanso58@gmail.com>
2023-09-24 15:50:28 +0800

    Turple -1

--- turple_ds.py
+++ turple_ds.py
@@ -2,5 +2,8 @@ midfielders = ("Kante", "Modric", "DeBrugner")
 print(f'Midfielders: {midfielders}')
 
 # list inside a turple
+mixed = ("Apple",[11,22,33])
+print(f'Nixed : {mixed}')
+
 
 # methods and operation.
