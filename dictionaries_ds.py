5d9ab020e8e40c9d387b260378ab587c31c2c29d
pythagoras58 <solomondanso58@gmail.com>
2023-09-27 23:09:35 +0800

    Dictionary Type

--- dictionaries_ds.py
+++ dictionaries_ds.py
@@ -1,6 +1,13 @@
 player_numbers = {
-    "Messi" : 10,
-    "Ronaldo" : 7,
-    "Neymar" : 11,
-    "Ramos" : 4
+    "Messi": 10,
+    "Ronaldo": 7,
+    "Neymar": 11,
+    "Ramos": 4,
+    "Silva": 2
 }
+
+free_agents = dict({"De Gea": 1, "Mamoudu": 2})
+
+print(f"Dictionary of Players: {player_numbers}")
+print(f'Type of : {type(free_agents)}')
+print(f'Type of : {type(player_numbers)}')
---
+++ numerical_values.py
@@ -0,0 +1,10 @@
+# Creating a range of numbers
+for value in range(1,4):
+    print(value)
+
+#  make a list out of a range function
+numbers = list(range(1,4))
+print(f'List of numbers: {numbers}')
+
+even_numbers = list(range(2,11,2))
+print(f'Even number list: {even_numbers}')
