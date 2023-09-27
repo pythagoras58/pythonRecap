942b060e393e333ee2f9aa62d19e8b8373bf5b8a
pythagoras58 <solomondanso58@gmail.com>
2023-09-27 23:27:37 +0800

    Dictionary Operations

--- dictionaries_ds.py
+++ dictionaries_ds.py
@@ -11,3 +11,7 @@ free_agents = dict({"De Gea": 1, "Mamoudu": 2})
 print(f"Dictionary of Players: {player_numbers}")
 print(f'Type of : {type(free_agents)}')
 print(f'Type of : {type(player_numbers)}')
+
+#  operations and methods
+print(f'Messi is number : {player_numbers["Messi"]}')
+print(f'Neymar is number : {player_numbers.get("Neymar")}')
