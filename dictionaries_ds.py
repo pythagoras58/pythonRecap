2256c6b6f5e47cb61a411557d19e08fa94af6891
pythagoras58 <solomondanso58@gmail.com>
2023-09-28 00:12:07 +0800

    Dictionary Operations - 3

--- dictionaries_ds.py
+++ dictionaries_ds.py
@@ -18,3 +18,7 @@ print(f'Neymar is number : {player_numbers.get("Neymar")}')
 
 fruit_price = {"Apple":[5,4,3], "Banana": 4}
 print(f'Apple List: {fruit_price["Apple"][1]}')
+
+#  methods
+fruit_price["Banana"] = 10 # update the value of a dictionary
+print(fruit_price)
