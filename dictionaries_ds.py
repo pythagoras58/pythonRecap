9acfff01764c2fdf3cbb1b744c750b24ec0caac1
pythagoras58 <solomondanso58@gmail.com>
2023-09-27 23:32:44 +0800

    Dictionary Operations - 2

--- dictionaries_ds.py
+++ dictionaries_ds.py
@@ -15,3 +15,6 @@ print(f'Type of : {type(player_numbers)}')
 #  operations and methods
 print(f'Messi is number : {player_numbers["Messi"]}')
 print(f'Neymar is number : {player_numbers.get("Neymar")}')
+
+fruit_price = {"Apple":[5,4,3], "Banana": 4}
+print(f'Apple List: {fruit_price["Apple"][1]}')
