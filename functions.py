314f99418147ea22a9dc46a3f43579d49315d321
pythagoras58 <solomondanso58@gmail.com>
2023-09-29 07:23:42 +0800

    Functions - 2

---
+++ functions.py
@@ -0,0 +1,18 @@
+# breaking programs into smaller chunks - function
+
+def hello_func() :
+    print("Say Hello")
+
+
+# call the function
+hello_func()
+
+
+# Arguments in functions
+def hello_user(name):
+    print(f'Hello, {name}')
+
+
+hello_user('Messi')
+hello_user('Ronaldo')
+hello_user('Salah')
